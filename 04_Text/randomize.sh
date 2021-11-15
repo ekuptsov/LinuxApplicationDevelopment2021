5f45a67a1a34eda6f2082084781659a77c5782f0
ekuptsov <s02150138@stud.cs.msu.ru>
2021-11-15 17:31:36 +0300

    UCS-2 support (Russian chars too)

--- 04_Text/randomize.sh
+++ 04_Text/randomize.sh
@@ -1,27 +1,37 @@
 #!/bin/sh
+
+# /bin/echo -e "п" | iconv -f UTF8 -t UCS-2 | for i in $(hexdump -v -e '2/1 "\\x%02x"" "'); do /bin/echo -e -n $i | iconv -f UCS-2 -t UTF8; done
+# transparent foreground on white background
+# echo -e "\e[38;5;0;48;5;255m{text}\e[0m"
+
 tput reset
 delay=$1
-RES=""
-i=0
-j=0
-for p in $(hexdump -v -e '1/1 "%x "')
+TEXT=""
+pos_i=0
+pos_j=0
+
+for rune in $(iconv -f UTF8 -t UCS-2 | hexdump -v -e '2/1 "\\x%02x"" "')
 do
-    RES="$RES$i\t$j\t\x$p\n"
-    j=$(($j + 1))
-    if [ "$p" = "a" ]
+    TEXT="$TEXT$pos_i\t$pos_j\t$rune\n"
+    pos_j=$(($pos_j + 1))
+    if [ "$rune" = "\x0a\x00" ]
     then
-        i=$(($i + 1))
-        j=0
+        pos_i=$(($pos_i + 1))
+        pos_j=0
     fi
 done
+last_i=$pos_i
+last_j=$pos_j
 
-printf "$RES" | shuf | while read -r piece
+printf "$TEXT" | shuf | while read -r piece
 do
     sleep $delay
-    ii=$(echo "$piece" | cut -f 1)
-    jj=$(echo $piece | cut -d' ' -f 2)
-    hx=$(echo "$piece" | cut -f 3)
-    tput cup $ii $jj
-    /bin/echo -e $hx
+
+    pos_i=$(echo "$piece" | cut -f 1)
+    pos_j=$(echo "$piece" | cut -f 2)
+    rune=$(echo "$piece"  | cut -f 3)
+
+    tput cup $pos_i $pos_j
+    /bin/echo -en "$rune" | iconv -f UCS-2 -t UTF8
 done
-tput cup $i $j
+tput cup $last_i $last_j
